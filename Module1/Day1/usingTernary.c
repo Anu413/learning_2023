80542f00fd67ccfa13d7e7f8aca83f331191ea46
Anu413 <anushau906@gmail.com>
2023-06-18 00:26:53 +0530

    using ternary operator

---
+++ Module1/Day1/usingTernary.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter first number:");
+    scanf("%d",&a);
+    printf("enter second number:");
+    scanf("%d",&b);
+    (a>b)?printf("first number is big"):printf("Second number is big");
+    return 0;
+}
